922c9fb62c5d538f9d9f897b9e76cbc26423e9bc
Antoine <heleineantoine@gmail.com>
2024-11-11 17:45:11 +0100

    First commit

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import './css/Header.css'
+
+function Header() {
+    // state
+    
+    // comportements
+
+    // affichage
+    return <div>
+            <nav  class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logo</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    </div>
+}
+
+
+export default Header
